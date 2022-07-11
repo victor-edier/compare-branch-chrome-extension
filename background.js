184a61247876060f341515347e1964889b27c50c
Victor Edier <edier.urrea@aviation.ink>
2022-07-11 12:54:04 -0500

    Fix the search

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.action.onClicked.addListener(tab => {
   const url = new URL(tab.url);
   if (url.hostname == 'github.com') {
-    if (url.pathname && url.pathname.search('/tree/')) {
+    if (url.pathname && url.pathname.search('/tree/') != -1) {
       let newUrl = url.href.replace('/tree/', '/compare/')
       chrome.tabs.update(tab.id, { url: newUrl });
     }
